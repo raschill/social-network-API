95bd9a363f8a91f155f08e1c6bd7a53225be4d09
raschill <rebeccaaschilling@gmail.com>
2024-12-26 18:29:41 -0600

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import espress from 'express';
+// import db from './config/connection.js';
+// import router from './routes/index.js';
+
+const PORT= 3001;
+const app= express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(router);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log('Server is running on port 3001.');
+    })
+})
